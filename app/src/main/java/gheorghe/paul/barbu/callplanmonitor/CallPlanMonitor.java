61bc2385914d394d33a317ae30c0bf53a0a53274
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2015-08-22 15:36:36 +0300

    implemented the counting as my carrier does

--- app/src/main/java/gheorghe/paul/barbu/callplanmonitor/CallPlanMonitor.java
+++ app/src/main/java/gheorghe/paul/barbu/callplanmonitor/CallPlanMonitor.java
@@ -62,11 +62,11 @@ public class CallPlanMonitor {
         Uri allCalls = Uri.parse("content://call_log/calls");
         Cursor c = context.getContentResolver().query(allCalls,
                 new String[]{CallLog.Calls.NUMBER, CallLog.Calls.DURATION, CallLog.Calls.TYPE},
-                CallLog.Calls.NUMBER + " NOT LIKE ? AND " + CallLog.Calls.NUMBER + " NOT LIKE ? AND " +
-                        CallLog.Calls.NUMBER + " NOT LIKE ? AND " + CallLog.Calls.NUMBER + " NOT LIKE ? AND " +
+//                CallLog.Calls.NUMBER + " NOT LIKE ? AND " + CallLog.Calls.NUMBER + " NOT LIKE ? AND " +
+//                        CallLog.Calls.NUMBER + " NOT LIKE ? AND " + CallLog.Calls.NUMBER + " NOT LIKE ? AND " +
                         CallLog.Calls.NUMBER + " NOT LIKE ? AND " + CallLog.Calls.DATE + " >= ?",
-                new String[]{"072%", "073%", "+4073%", "+4072%", "*%", cal.getTimeInMillis() + ""},
-                CallLog.Calls.DATE + " DESC");
+                new String[]{/*"072%", "073%", "+4073%", "+4072%", */"*%", cal.getTimeInMillis() + ""},
+                CallLog.Calls.DATE + " ASC");
 
         int durationIndex = c.getColumnIndex(CallLog.Calls.DURATION);
         int typeIndex = c.getColumnIndex(CallLog.Calls.TYPE);
@@ -82,13 +82,35 @@ public class CallPlanMonitor {
             if(type == CallLog.Calls.OUTGOING_TYPE) {
                 double realDuration = 0;
                 if(duration > 60) {
-                    realDuration = Math.ceil(duration / 60);
+                    realDuration = Math.ceil(duration / 60.0);
                 }
                 else { // the first several seconds of the first minute are billed as one minute
                     realDuration = 1;
                 }
 
-                sum += realDuration;
+                if(sum >= 25)
+                {
+                    // above the 25 minutes limit, only national calls are billed
+                    if(!number.startsWith("072") && !number.startsWith("073") && !number.startsWith("+4073")
+                            && !number.startsWith("+4072"))
+                    {
+                        sum += realDuration;
+                    }
+                }
+                else {
+                    if((number.startsWith("072") || number.startsWith("073") || number.startsWith("+4073")
+                            || number.startsWith("+4072")) && sum + realDuration > 25)
+                    {
+                        // if the number being called is local, but it pushes the total above 25 minutes,
+                        // the rest will not be billed by the carrier since it's a local number
+                        // and the 25 (special) minutes are exhausted now
+                        sum = 25;
+                    }
+                    else { // otherwise the call should be logged normally inside the 25 minutes limit
+                        sum += realDuration;
+                    }
+                }
+
                 sb.append("\n " + number + ": " + realDuration);
             }
         }
